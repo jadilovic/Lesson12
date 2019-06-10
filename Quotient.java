83c7061d041fcb910078821b6d28c4b42c341a8e
jadilovic <adilovic79@yahoo.com>
2019-06-10 22:17:12 +0200

    second java commit

--- Quotient.java
+++ Quotient.java
@@ -1,9 +1,19 @@
 package vjezba11_1;
 
+import java.util.Scanner;
+
 public class Quotient {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter two numbers");
+		int num1 = input.nextInt();
+		int num2 = input.nextInt();
+		
+		System.out.println("Number " + num1 + " divided by " + num2 + " is " + num1 / num2);
 
 	}
 
