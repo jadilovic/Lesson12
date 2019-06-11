ae1b5c5eea354faa1ca0dc79b29d9750e6eb9ec5
jadilovic <adilovic79@yahoo.com>
2019-06-11 20:34:33 +0200

    exception handling added

--- Quotient.java
+++ Quotient.java
@@ -13,8 +13,10 @@ public class Quotient {
 		int num1 = input.nextInt();
 		int num2 = input.nextInt();
 		
+		if(num2 != 0)
 		System.out.println("Number " + num1 + " divided by " + num2 + " is " + num1 / num2);
-
+		else
+			System.out.println("Divisor cannot be zero");
 	}
 
 }
